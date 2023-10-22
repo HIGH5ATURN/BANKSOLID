ecf48cb34828cd9a78b6acb157f6f1772f3c5d72
ONI <rowshan@iut-dhaka.edu>
2023-10-22 10:38:49 +0600

    First commit

---
+++ BANKSOLID/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANKSOLID
+{
+    public abstract class Account
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public double Balance { get; set; }
+
+
+    }
+}
---
+++ BANKSOLID/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANKSOLID
+{
+    
+
+    public class Customer
+    {
+        public String Name { get; set; }
+        public String NID { get; set; }
+
+        public List<Account> Accounts { get; set; }
+        
+        public string PassWord { get; set; }
+        public Customer(String name, String NID, string passWord)
+        {
+            Name = name;
+            this.NID = NID;
+            PassWord = passWord;
+        }
+
+    }
+}
